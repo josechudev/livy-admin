3838246f2b01dbfc29c9348dd3e9dc9fa969fee7
Jose <jose.chu.dev@gmail.com>
2023-01-16 16:47:54 -0500

    basic auth

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ---
 Livy admin is an api + web ui for managing sessions and permissions for Apache Livy API
 
+```
+docker run -p 80:80 -it --rm -v ${PWD}:/work -w /work --entrypoint /bin/sh python:3.9-alpine
+
+uvicorn app.main:app --port 80 --host 0.0.0.0 --reload
+```
--- api/app/main.py
+++ api/app/main.py
@@ -1,15 +1,48 @@
-from typing import Union
 
-from fastapi import FastAPI
+import fastapi
+import httpx
+import starlette
+
+from fastapi import FastAPI, Request
+from fastapi.responses import StreamingResponse
+
+from starlette.background import BackgroundTask
+
+from fastapi import Depends, status, HTTPException
+import os
+import secrets
+
+from fastapi import status, HTTPException
+from fastapi.security import OAuth2PasswordBearer, api_key
+
+import app.auth as auth
+
+
+SERVER = "https://virtserver.swaggerhub.com/josechudev/livy/1.0.0"
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+client = httpx.AsyncClient(base_url=SERVER)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home():
+    return {"message": "Livy server"}
+@app.get("/sessions")
+async def get_sessions(api_key: api_key.APIKey = Depends(auth.get_api_key)):
+    req = client.build_request('GET', SERVER + '/sessions')
+    rp_resp = await client.send(req)
+    return fastapi.responses.JSONResponse(content=rp_resp.json())
+
+async def _reverse_proxy(request: Request):
+    url = httpx.URL(path=request.url.path, query=request.url.query.encode("utf-8"))
+    rp_req = client.build_request(
+        request.method, url, headers=request.headers.raw, content=await request.body()
+    )
+    rp_resp = await client.send(rp_req)
+    return fastapi.responses.JSONResponse(content=rp_resp.json())
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+app.add_route("/{path:path}", _reverse_proxy, ["GET", "POST"])
